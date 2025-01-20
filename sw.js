1d2d07b1eecd1646f6dbe6fe196387cd4a181854
Michael Kolesidis <michael.kolesidis@gmail.com>
2025-01-20 18:12:53 +0200

    add service worker for offline pwa

---
+++ sw.js
@@ -0,0 +1,53 @@
+/*
+ *  Pixelated
+ *  Copyright (c) Michael Kolesidis <michael.kolesidis@gmail.com>
+ *  GNU Affero General Public License v3.0
+ */
+
+const CACHE_NAME = 'pixelated-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/pixelated.min.js',
+  '/texts.js',
+  '/assets/favicon.ico',
+  '/assets/apple-touch-icon.png',
+  '/assets/logo.svg',
+  // Add other assets here
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
